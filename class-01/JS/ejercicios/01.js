de5260ce1b60a744d72c4f146fa439a153a1c09f
Wilmar Garcia <wilmar.garciava@gmail.com>
2024-09-23 20:51:03 -0500

    feat(js->ejercicios) implementación y conversión de datos y templates literals

---
+++ class-01/JS/ejercicios/01.js
@@ -0,0 +1,9 @@
+const nombre = "Juan";
+let edad = 23;
+const isDeveloper = true;
+
+const edadString = String(edad);
+const edadNumber = Number(edadString);
+
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y soy desarrollador: ${isDeveloper}`);
+console.log("Hola, mi nombre es " + nombre + ", tengo " + edad + " años y soy desarrollador: " + isDeveloper);
